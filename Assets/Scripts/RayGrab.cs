3ade54a656f121398c1b27e97a0482753d61d98e
sehajbajaj2004 <sehajbajaj2004@gmail.com>
2025-08-02 21:40:40 +0530

    ray grabbing

---
+++ Assets/Scripts/RayGrab.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RayGrabXZOnly : MonoBehaviour
+{
+    public XRRayInteractor rayInteractor;
+    public Transform attachPoint;
+
+    private void Awake()
+    {
+        if (rayInteractor == null)
+            rayInteractor = GetComponent<XRRayInteractor>();
+    }
+
+    private void Update()
+    {
+        if (rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit))
+        {
+            Vector3 hitPos = hit.point;
+            hitPos.y = 0; // lock to XZ plane
+            attachPoint.position = hitPos;
+        }
+    }
+}
